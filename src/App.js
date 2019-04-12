840b99e84ebd40c89f0eb16e545c0c7f5d95975b
unknown <mirukusheki@gmail.com>
2019-04-12 13:38:27 +0700

    refoldering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component}     from 'react';
+import {HashRouter, Route} from "react-router-dom";
+import Home                   from './routes/Home/Home';
+import About                  from "./routes/About/About";
+
+import "./styles/index.scss";
 
 class App extends Component {
+  routes = [
+    {
+      path     : '/',
+      exact    : true,
+      component: Home
+    },
+    {
+      path     : '/about',
+      exact    : true,
+      component: About
+    },
+  ];
+
   render() {
     return (
-      <div className="App">
-        Hello world
-      </div>
+      <HashRouter>
+        <div className="App">
+          {this.routes.map((route, i) => <Route {...route}/>)}
+        </div>
+      </HashRouter>
     );
   }
 }

e6fece173177d9b60433efe1ceb025ca12b0a461
Ryan Jonathan <ryan@kyou.id>
2019-05-17 12:57:46 +0700

    Restyling download doujin button

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,54 +1,54 @@
-import React, {Component} from 'react';
-import {Button, Spinner}  from 'react-bootstrap';
-import axios              from 'axios';
+import React, { Component } from "react";
+import { Button, Spinner } from "react-bootstrap";
+import axios from "axios";
 
-import Assets from '../../assets';
-import './Home.scss';
+import Assets from "../../assets";
+import "./Home.scss";
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       number: 0,
-      show  : true,
+      show: true,
       detail: {
-        title    : '',
-        tags     : [],
-        thumbnail: '',
+        title: "",
+        tags: [],
+        thumbnail: "",
         isLoading: false,
-        isError  : false
+        isError: false
       }
-    }
+    };
   }
 
   generateCode = () => {
-    const min    = 1;
-    const max    = 269000;
-    const rand   = min + Math.random() * (max - min);
+    const min = 1;
+    const max = 269000;
+    const rand = min + Math.random() * (max - min);
     const number = Math.floor(rand);
-    this.setState({number: number});
+    this.setState({ number: number });
     this.getMeta(number);
   };
 
   async getMeta(num) {
     this.setState({
       detail: {
-        title    : '',
-        tags     : [],
-        thumbnail: '',
+        title: "",
+        tags: [],
+        thumbnail: "",
         isLoading: true,
-        isError  : false
+        isError: false
       }
     });
     try {
-      let res         = await axios.get('https://nhtai-api.glitch.me/api/id?id=' + num);
-      const media     = res.data.media_id;
-      const cover     = res.data.images.cover;
+      let res = await axios.get("https://nhtai-api.glitch.me/api/id?id=" + num);
+      const media = res.data.media_id;
+      const cover = res.data.images.cover;
       const thumbnail = this.computeCoverUrl(media, cover);
       this.setState({
         detail: {
           title: res.data.title.english,
-          tags : res.data.tags,
+          tags: res.data.tags,
           thumbnail
         }
       });
@@ -56,81 +56,119 @@ class Home extends Component {
       this.setState({
         meta: {
           isLoading: false,
-          isError  : true
+          isError: true
         }
       });
-      console.log('This is your error' + e);
+      console.log("This is your error" + e);
     }
-  };
+  }
 
   computeCoverUrl(media, image) {
     // make cover image scr url
-    const cover_type = {j: "jpg", p: "png"};
-    const this_type  = image.t;
-    return `https://kontol.nhent.ai/galleries/${media}/cover.${cover_type[this_type]}`;
+    const cover_type = { j: "jpg", p: "png" };
+    const this_type = image.t;
+    return `https://kontol.nhent.ai/galleries/${media}/cover.${
+      cover_type[this_type]
+    }`;
   }
 
   render() {
-    const {number, detail} = this.state;
+    const { number, detail } = this.state;
     return (
       <div className="container">
         <div className="title">
-          <img src={Assets.NHen} alt="nHentai Logo" onContextMenu={e => e.preventDefault()}/><br/>
+          <img
+            src={Assets.NHen}
+            alt="nHentai Logo"
+            onContextMenu={e => e.preventDefault()}
+          />
+          <br />
           <h1>NHentai Number Generator</h1>
           <i>Your pleasure, my pleasure! No, it's OUR PLEASURE!</i>
         </div>
         <div className="generate">
-          <Button variant="light" onClick={e => this.generateCode()}>Generate Number</Button><br/>
-          {
-            number !== 0 ?
-              <div className="row message">
-                <div className="col-12 col-md-6">
-                  <b>
-                    This is your nHentai number:<br/>
-                    Click here to access the doujin: <br/>
-                    Click here to download the doujin: <a href={`https://dl.nhent.ai/dl/${number}`} target="_blank" rel="noopener noreferrer">Download</a><br/>
-                  </b>
-                  <a href={`https://nhentai.net/g/${number}`} target="_blank" rel="noopener noreferrer">
-                    {number}
+          <Button variant="light" onClick={e => this.generateCode()}>
+            Generate Number
+          </Button>
+          <br />
+          {number !== 0 ? (
+            <div className="row message">
+              <div className="col-12 col-md-6">
+                <b>
+                  This is your nHentai number:
+                  <br />
+                  Click here to access the doujin: <br />
+                  <br />
+                </b>
+                <a
+                  href={`https://nhentai.net/g/${number}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="number"
+                >
+                  {number}
+                </a>
+                <br />
+                Use VPN to access that website if your provider blocks.
+                <div className="mirror">
+                  <a
+                    href={`https://nhent.ai/g/${number}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Click here if you want to access without VPN
                   </a>
-                  <br/>
-                  Use VPN to access that website if your provider blocks.
-                  <div className="mirror">
-                    <a href={`https://nhent.ai/g/${number}`} target="_blank" rel="noopener noreferrer">
-                      Click here if you want to access without VPN
-                    </a>
-                  </div>
                 </div>
-                <div className="col-12 col-md-6">
-                  {detail.isLoading ? <Spinner animation="grow"/> :
-                    <div className="doujin">
-                      {detail.isError ? 'Preview Not Available' :
-                        <div className="doujin-info">
-                          <b>Doujin Info</b> <br/>
-                          <div className="row">
-                            <div className="col-6 col-md-4">
-                              <a href={`https://nhentai.net/g/${number}`} target="_blank" rel="noopener noreferrer">
-                                <img src={detail.thumbnail} alt="thumbnail"/>
-                              </a>
-                            </div>
-                            <div className="col-6 col-md-8">
-                              Title <br/>
-                              {detail.title}<br/>
-                              Tags <br/>
-                              {detail.tags.map((item, i) => <span key={i}>{item.name}, </span>)}<br/>
-                            </div>
+              </div>
+              <div className="col-12 col-md-6">
+                {detail.isLoading ? (
+                  <Spinner animation="grow" />
+                ) : (
+                  <div className="doujin">
+                    {detail.isError ? (
+                      "Preview Not Available"
+                    ) : (
+                      <div className="doujin-info">
+                        <b>Doujin Info</b> <br />
+                        <div className="row">
+                          <div className="col-6 col-md-4">
+                            <a
+                              href={`https://nhentai.net/g/${number}`}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <img src={detail.thumbnail} alt="thumbnail" />
+                            </a>
+                          </div>
+                          <div className="col-6 col-md-8">
+                            Title <br />
+                            {detail.title}
+                            <br />
+                            <br />
+                            Tags <br />
+                            {detail.tags.map((item, i) => (
+                              <span key={i}>{item.name}, </span>
+                            ))}
+                            <br />
+                            <a
+                              href={`https://dl.nhent.ai/dl/${number}`}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="btn btn-primary"
+                            >
+                              Download The Doujin
+                            </a>
                           </div>
                         </div>
-                      }
-                    </div>
-                  }
-                </div>
+                      </div>
+                    )}
+                  </div>
+                )}
               </div>
-              :
-              <div className="message">
-                Please click "Generate Number" button
-              </div>
-          }
+            </div>
+          ) : (
+            <div className="message">Please click "Generate Number" button</div>
+          )}
         </div>
       </div>
     );
